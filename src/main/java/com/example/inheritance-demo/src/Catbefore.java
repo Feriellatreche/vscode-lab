04f206a6a5ef1d36a37362f06eecf3bdc5242fea
Feriellatreche <your.gymdzgymdz@gmail.com>
2026-04-28 20:00:52 +0100

    feat: before refactoring - duplicate eat/sleep methods

---
+++ src/main/java/com/example/inheritance-demo/src/Catbefore.java
@@ -0,0 +1,13 @@
+public class Catbefore {
+    public void eat() {
+        System.out.println("Cat is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Cat is sleeping");
+    }
+
+    public void meow() {
+        System.out.println("Meow!");
+    }
+}
---
+++ src/main/java/com/example/inheritance-demo/src/Dogbefore.java
@@ -0,0 +1,13 @@
+public class Dogbefore {
+    public void eat() {
+        System.out.println("Dog is eating");
+    }
+
+    public void sleep() {
+        System.out.println("Dog is sleeping");
+    }
+
+    public void bark() {
+        System.out.println("Woof!");
+    }
+}
